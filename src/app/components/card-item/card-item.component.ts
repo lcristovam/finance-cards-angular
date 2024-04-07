0aa888e37eb8c2f8a803430e15fbb4e8513e22f9
Lucas de Moura <lucas@CTNOT01.clamom.local>
2024-04-07 18:52:24 -0300

    First commit

---
+++ src/app/components/card-item/card-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.scss']
+})
+export class CardItemComponent {
+
+    @Input () text: string = '';
+    @Input () value : string= "";
+
+}
